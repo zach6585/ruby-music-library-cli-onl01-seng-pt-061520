50a1fb1efccf34f803f8536ea6712c1d93b5670f
zachary mines <zmines@wesleyan.edu>
2020-07-26 22:57:41 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -1,8 +1,16 @@
 class MusicLibraryController
   attr_accessor :path
-  def initialize(path = nil)
+  def initialize(path = './db/mp3s')
     @path = path
     mi = MusicImporter.new(@path)
     mi.import
+  end 
+  
+  def call 
+    puts 'Welcome to your music library!'
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list aritst'."
   end 
 end 
